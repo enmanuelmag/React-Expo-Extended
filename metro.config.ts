428fcbdb695a1d11fd6f8770603b52b74b977ab1
enmanuelmag <enmanuelmag@cardor.dev>
2025-04-05 18:16:04 -0500

    feat: update logic

--- metro.config.ts
+++ metro.config.ts
@@ -9,6 +9,12 @@ module.exports = (() => {
     transformer: {
       ...transformer,
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      // getTransformOptions: async () => ({
+      //   transform: {
+      //     experimentalImportSupport: false,
+      //     inlineRequires: false,
+      //   },
+      // }),
     },
     resolver: {
       ...resolver,
@@ -17,3 +23,23 @@ module.exports = (() => {
     },
   };
 })();
+
+// import { getDefaultConfig } from 'expo/metro-config';
+
+// export default (() => {
+//   const config = getDefaultConfig(__dirname);
+
+//   const { transformer, resolver } = config;
+
+//   config.transformer = {
+//     ...transformer,
+//     babelTransformerPath: require.resolve('react-native-svg-transformer'),
+//   };
+//   config.resolver = {
+//     ...resolver,
+//     assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+//     sourceExts: [...resolver.sourceExts, 'svg'],
+//   };
+
+//   return config;
+// })();
